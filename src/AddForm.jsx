5351192dea4ca83575436a17bd4f5c841155dede
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-16 10:01:59 +0800

    Track the addForm input state

--- src/AddForm.jsx
+++ src/AddForm.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import Reac, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function AddForm() {
+	const [newGrocery, setNewGrocery] = useState("");
 	return (
 		<section>
 			<form className="add-form">
-				<input type="text" placeholder="Groceries to buy" />
-                <button><FontAwesomeIcon icon={faPlus}/></button>
+				<input
+					type="text"
+					placeholder="Groceries to buy"
+					value={newGrocery}
+					onChange={(e) => setNewGrocery(e.target.value)}
+				/>
+				<button>
+					<FontAwesomeIcon icon={faPlus} />
+				</button>
 			</form>
 		</section>
 	);
